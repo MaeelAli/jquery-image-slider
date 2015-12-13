c352101e6ca2664060588d409b23626fb277567c
Ismaeel <maeelali92@gmail.com>
2015-12-13 22:21:34 +0000

    added check if at end of images reset from begginning

--- js/slider.js
+++ js/slider.js
@@ -9,10 +9,23 @@ $(document).ready(function() {
 function startSlider() {
   //get the number of images
   var count = $('#slider > img').size();
+  
+  //autoloop through available images
   var loop = setInterval(function() {
+    
+    //if sliderNext is greater than # images then reset to 1
+    if (sliderNext > count) {
+      sliderNext = 1;
+      sliderInt = 1;
+      
+    }
+    //fadeOut all images
     $('#slider > img').fadeOut(300);
+    
+    //fadeIn next image based on id
     $('#slider > img#' + sliderNext).fadeIn(300);
     
+    //update counter variables to keep track of looping
     sliderInt = sliderNext;
     sliderNext = sliderNext + 1;
   }, 3000)
