e9240b15adcf55b5e7e79839aef10abf7a926330
Ismaeel <maeelali92@gmail.com>
2015-12-13 22:16:55 +0000

    fadeIn from 1..2..3....n

--- js/slider.js
+++ js/slider.js
@@ -3,4 +3,17 @@ var sliderNext = 2;
 
 $(document).ready(function() {
   $('#slider >img#1').fadeIn(300);
+  startSlider();
 });
+
+function startSlider() {
+  //get the number of images
+  var count = $('#slider > img').size();
+  var loop = setInterval(function() {
+    $('#slider > img').fadeOut(300);
+    $('#slider > img#' + sliderNext).fadeIn(300);
+    
+    sliderInt = sliderNext;
+    sliderNext = sliderNext + 1;
+  }, 3000)
+}
